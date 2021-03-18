955da71c4cc1b7188af9245a58ddef77ec264a4d
MaakeMotalane <53600843+MaakeMotalane@users.noreply.github.com>
2021-03-18 13:27:46 +0200

    m

---
+++ pdf.js
@@ -0,0 +1,25 @@
+const pdf = require("pdf-creator-node");
+const fs = require("fs");
+
+const template = fs.readFileSync("./template/template.html", "utf-8");
+const options = {
+    format: "A4", 
+    orientantion: "portrait",
+    border: "15mm",
+};
+
+const document = {
+    html: template,
+    data: {
+        message: "referenceNum",
+    },
+    path: "./notifications.pdf",
+
+};
+
+pdf.create(document, options).then((res)=>{
+    console.log(res);
+})
+.catch((err) => {
+    console.log(err);
+});
